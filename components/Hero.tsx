4211d0dcbbfb6f1298a1461b7f960465fa22fbf7
Josh McLain <joshmclain45@gmail.com>
2022-10-29 23:26:37 -0700

    finished hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import UserAvatar from "public/assets/img/avatar-370.svg";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hello Universe, the name is Josh.",
+      "const iDoCode: boolean = true;",
+      "CoolCodingCat.tsx",
+    ],
+    loop: true,
+    delaySpeed: 3000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <motion.div
+        initial={{
+          rotateX: 0,
+          opacity: 0,
+          scale: 3,
+        }}
+        animate={{
+          rotateX: 720,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 3.5,
+        }}
+      >
+        <Image
+          className="relative h-32 w-32 mx-auto rounded-full object-cover"
+          src={UserAvatar}
+          alt="Josh Avatar"
+        />
+      </motion.div>
+      <div className="z-20">
+        <h2 className="text-small uppercase text-gray-400 tracking-[8px] pb-2">
+          {"<Software Engineer />"}
+          <br />
+          {"<Web Developer />"}
+        </h2>
+        <h1 className="text-3xl lg:text-6xl md:text-5xl sm:text-4xl font-semibold px-10">
+          <span className="text-white mr-3">{text}</span>
+          <Cursor cursorColor="green" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
-import styles from "../styles/Home.module.css";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
-      {/* header */}
       {/* hero*/}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+      {/* ABout */}
       {/* Experience */}
       {/* skills */}
       {/* projects */}
