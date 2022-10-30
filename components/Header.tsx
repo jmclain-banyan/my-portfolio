2807bc3849addb4b993a5bcb3f96ccb8fd5b87d1
Josh McLain <joshmclain45@gmail.com>
2022-10-29 20:20:38 -0700

    added framer motion and animated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://github.com/jmclain-banyan"
           fgColor="gray"
@@ -22,16 +38,33 @@ const Header = (props: Props) => {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
-      <div className="flex flex-row items-center cursor-pointer text-gray-300">
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center cursor-pointer text-gray-300"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
         />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">contact me</p>
-      </div>
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          contact me
+        </p>
+      </motion.div>
     </header>
   );
 };
