873e511f694079d4818a1f66d509f4b5ee40539e
Josh McLain <joshmclain45@gmail.com>
2022-12-22 20:31:47 -0700

    added contact section

---
+++ components/ContactMe.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Props = {};
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+const ContactMe = (props: Props) => {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    window.location.href = `mailto:joshmclain45@gmail.com?subject=${data.subject}
+    &body=Hi my name is ${data.name},\n${data.message}\n\n${data.email}`
+  };
+
+  return (
+    <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact me
+      </h3>
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          I&apos;ve got just what you need.{" "}
+          <span className="decoration-[#88d498]/50 underline">Lets talk.</span>
+        </h4>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#88d498] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+1234567890</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#88d498] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">sparky@sharklasers.net</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#88d498] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">Phoenix, AZ</p>
+          </div>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+            <input {...register('email')} placeholder="Email" className="contactInput" type="email" />
+          </div>
+          <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
+          <textarea {...register('message')} placeholder="Message" className="contactInput" />
+          <button
+            type="submit"
+            className="bg-[#88d498]/50 py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import About from 'components/About';
+import About from "components/About";
 import WorkExperience from "components/WorkExperience";
 import Skills from "components/Skills";
 import Projects from "components/Projects";
+import ContactMe from "components/ContactMe";
 
 export default function Home() {
   return (
@@ -13,27 +14,24 @@ export default function Home() {
         <title>Create Next App</title>
       </Head>
       <Header />
-      {/* hero*/}
       <section id="hero" className="snap-start">
         <Hero />
       </section>
-      {/* ABout */}
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* Experience */}
-      <section id='experience' className="snap-center">
+      <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
-      {/* skills */}
-      <section id='skills' className="snap-start">
+      <section id="skills" className="snap-start">
         <Skills />
       </section>
-      {/* projects */}
-      <section id='projects' className="snap-start">
+      <section id="projects" className="snap-start">
         <Projects />
       </section>
-      {/* contact me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   );
 }
